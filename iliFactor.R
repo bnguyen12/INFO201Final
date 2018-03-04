296487d52beb30e49640fbd517ca21c3fd53dac4
Kevin <kevinmcclenahan77@gmail.com>
2018-03-03 23:06:42 -0800

    Switched over to data set for all deaths in given year span. Added a slider for week input that can alter the graph based on user input and switched trend selector to a year selector that the week slider will be based on. Need to add interactive map features for user hover actions.

--- iliFactor.R
+++ iliFactor.R
@@ -2,7 +2,7 @@ source("setup.R")
 
 ili.data.2015.2016 <- read.csv("./data/Washington_FluView_BySeason_2015-2016.csv", stringsAsFactors = FALSE)
 ili.data.2017.2018 <- read.csv("./data/Washington_FluView_BySeason_2017-2018.csv", stringsAsFactors = FALSE)
-flu.mortality.2015.2018 <- read.csv("./data/PedFluDeath_WeeklyData_2015-2018.csv", stringsAsFactors = FALSE)
+flu.mortality.2015.2018 <- read.csv("./data/State_Washington_2015-18_Data.csv", stringsAsFactors = FALSE)
 
 ili.data <- full_join(ili.data.2015.2016, ili.data.2017.2018)
 
@@ -15,8 +15,7 @@ ili.data <- ili.data %>% select(YEAR, WEEK, ILITOTAL, TOTAL.PATIENTS,
 #Contains flu mortality as well as ili reported cases
 flu.mortality.ili.data <- na.omit(full_join(flu.mortality.2015.2018, ili.data)) 
 
-flu.mortality.ili.data <- flu.mortality.ili.data %>% mutate(DEATH_RATE = (PREVIOUS.WEEKS.DEATHS / 
-                                                                          YEAR.TOTAL.DEATHS) * 100)
+
 
 
 
@@ -26,14 +25,28 @@ flu.mortality.ili.data <- flu.mortality.ili.data %>% mutate(DEATH_RATE = (PREVIO
 my.ui <- fluidPage(
   
   sidebarLayout(
+  
     sidebarPanel(
-      selectInput("trend.select", "Select a Trend", choices = c("2015 Trend" ,"2016 Trend", "2017 Trend", 
-                              "Current Year"), selected = "")
+     
+      selectInput("year.select", "Select a Year", choices = c("2015" ,"2016", "2017", 
+                              "Current Year"), selected = "Current Year"),
+     
+      sliderInput("week.slider", "Display weeks of choice", 
+                  min = 1, max = 52, value = c("", ""), sep = 1)
       
       
     ),
     mainPanel(
-      titlePanel("Influenza like illnesses compared to Death Rates"),
+      
+      
+      titlePanel("Influenza like illnesses compared to death rates"),
+      p("This plot displays a visual of ili cases (influenza like illnesses) alongside a plot of the desired
+        year's deaths. Severity of any illness is best represented by the number of deaths it has caused, as
+        the death count with any illness is what shocks the media most. When this media coverage happens, it 
+        is statistically prevalent that more people are checking into hospitals for ili cases out of concern 
+        for having the actual flu. Therefor, the higher the death count, the greater the number of ili cases 
+        in any given year."),
+      
       plotOutput("ili.map")
       
     )
@@ -43,43 +56,62 @@ my.ui <- fluidPage(
   
 )
 
-my.server <- function(input, output) {
- 
+my.server <- function(input, output, session) {
+  
+  
+  
   output$ili.map <- renderPlot({
+    #Takes in a data frame, a string for a desired year, and two integers to specify a week range
+    #that is then used to make a plot graph for ili/death rate data for the selected year.
     
-    plot.builder <- function(data.frame){
+    plot.builder <- function(mortality.ili.data, year.select, user.week, week.begin, week.end){
+      
+      suppressWarnings(flu.mortality.ili.data.year <- mortality.ili.data %>% filter(YEAR == year.select,
+                                                                  WEEK >= user.week & WEEK <= week.end))
+      
+      
+      updateSliderInput(session, "week.slider", value = c(user.week, user.week), 
+                                         min = week.begin, max = week.end)
+      
       ili.plot <- ggplot(data = flu.mortality.ili.data.year, aes(x = WEEK, y = Percentage, group = YEAR)) +
-        geom_line(aes(y = PERCENT_UNWEIGHTED_ILI, color = "ili cases")) +
-        geom_line(aes(y = DEATH_RATE, color = "Deaths"))
+                  geom_line(aes(y = PERCENT_UNWEIGHTED_ILI, color = "ili cases")) +
+                  geom_line(aes(y = PERCENT.P.I, color = "Deaths"))
       return(ili.plot)
     }
     
-  if(input$trend.select == "2015 Trend"){
-    flu.mortality.ili.data.year <- flu.mortality.ili.data %>% filter(YEAR == "2015", (WEEK >= 48 & WEEK <= 52)) 
-    
-    plot.builder(flu.mortality.ili.data.2015)
-  }
-  #2-10 weeks for 2016 data trend
-  else if(input$trend.select == "2016 Trend"){
-    flu.mortality.ili.data.year <- flu.mortality.ili.data %>% filter(YEAR == "2016", (WEEK >= 2 & WEEK <= 10)) 
-    
-    plot.builder(flu.mortality.ili.data.year)
+  if(input$year.select == "2015"){
+    week.begin <- 40
+    week.end <- 52
+    user.week <- input$week.slider
+    plot.builder(flu.mortality.ili.data, "2015", user.week, week.begin, week.end)
     
   }
-   else if(input$trend.select == "2017 Trend"){
-      flu.mortality.ili.data.year <- flu.mortality.ili.data %>% filter(YEAR == "2017", (WEEK >= 49 & WEEK <= 52)) 
-      
-      plot.builder(flu.mortality.ili.data.year)
-      
-    }
-    else {
-      flu.mortality.ili.data.year <- flu.mortality.ili.data %>% filter(YEAR == "2018", (WEEK >= 2 & WEEK <= 7)) 
-      
-      plot.builder(flu.mortality.ili.data.year)
+  
+  else if(input$year.select == "2016"){
+    week.begin <- 1
+    week.end <- 52
+    user.week <- input$week.slider
+    plot.builder(flu.mortality.ili.data, "2016", user.week, week.begin, week.end)
+    
+  }
+  else if(input$year.select == "2017"){
+    week.begin <- 40
+    week.end <- 52
+    user.week <- input$week.slider
+    plot.builder(flu.mortality.ili.data, "2017", user.week, week.begin, week.end)
+    
+  }
+  else {
+      week.begin <- 1
+      week.end <- 5
+      user.week <- input$week.slider
+      plot.builder(flu.mortality.ili.data, "2018",user.week, week.begin, week.end)
       
     }
   
   }) 
+  
+
   
   
 }
