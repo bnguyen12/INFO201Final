ae7fda03529d3cef0a4d69f2e22793513d2714ae
Benny Nguyen <benny.nguyenx@gmail.com>
2018-03-05 11:02:38 -0800

    Condense a few lines of code and made a reactive variable for pediatric death count

--- pediatricDeathRate.R
+++ pediatricDeathRate.R
@@ -3,13 +3,23 @@ library("ggplot2")
 library("shiny")
 
 ped.flu.death.data <- read.csv("./data/PedFluDeath_MapData.csv")
-ped.flu.death.data$REGION <- as.character(ped.flu.death.data$REGION)
 
 my.ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput("region.select", "Select Region", c("1" ,"2", "3", "4", "5", "6", "7", "8", "9", "10"), selected=c("1" ,"2", "3", "4", "5", "6", "7", "8", "9", "10")),
-      selectInput("target.select", "Select target", c("Rate" ,"Count"), selected="Rate")
+      checkboxGroupInput("region.select", "Select Region", 
+                         c("1 - Connecticut, Maine, Massachusetts, New Hampshire, Rhode Island, and Vermont",
+                           "2 - New Jersey, New York, Puerto Rico, and the U.S. Virgin Islands",
+                           "3 - Delaware, District of Columbia, Maryland, Pennsylvania, Virginia, and West Virginia",
+                           "4 - Alabama, Florida, Georgia, Kentucky, Mississippi, North Carolina, South Carolina, and Tennessee",
+                           "5 - Illinois, Indiana, Michigan, Minnesota, Ohio, and Wisconsin", 
+                           "6 - Arkansas, Louisiana, New Mexico, Oklahoma, and Texas",
+                           "7 - Iowa, Kansas, Missouri, and Nebraska",
+                           "8 - Colorado, Montana, North Dakota, South Dakota, Utah, and Wyoming",
+                           "9 - Arizona, California, Hawaii, and Nevada",
+                           "10 - Alaska, Idaho, Oregon, and Washington"), 
+                         selected = c("10 - Alaska, Idaho, Oregon, and Washington")),
+      selectInput("target.select", "Select target", c("Rate" ,"Count"), selected = "Rate")
     ),
     mainPanel(
       titlePanel("Pediatric Death Rate In Different Region"),
@@ -19,14 +29,26 @@ my.ui <- fluidPage(
 )
 
 my.server <- function(input, output) {
+  
+  ped.flu.data <- reactive({
+    data <- ped.flu.death.data %>% filter(REGION %in% as.numeric(gsub("([0-9]+).*$", "\\1", input$region.select)))
+    return(data)
+  })
   output$plot <- renderPlot({
-    if (!is.null(input$region.select)) {
-        data <- ped.flu.death.data %>% filter(REGION %in% c(input$region.select))
-        if (input$target.select == "Rate") {
-            ggplot(data, aes(x=SEASON, y=RATE, group=REGION)) + geom_line(aes(color=REGION))  + xlab("Season") + ylab("Pediatric Death Rate")
-        } else {
-            ggplot(data, aes(x=SEASON, y=COUNT, group=REGION)) + geom_line(aes(color=REGION))  + xlab("Season") + ylab("Pediatric Death Count")
-        }
+    if (input$target.select == "Rate") {
+      ggplot(ped.flu.data(), aes(x=SEASON, y=RATE, group=REGION)) + 
+      geom_line(aes(color=as.factor(REGION))) + 
+      labs(title = "Pediatric Deaths by Region",
+           x = "Season",
+           y = "Pediatric Death Rate",
+           color = "Region")
+    } else {
+      ggplot(ped.flu.data(), aes(x=SEASON, y=COUNT, group=REGION)) +
+      geom_line(aes(color=as.factor(REGION))) +
+      labs(title = "Pediatric Deaths by Region",
+           x = "Season",
+           y = "Pediatric Death Count",
+           color = "Region")
     }
   }) 
 }
